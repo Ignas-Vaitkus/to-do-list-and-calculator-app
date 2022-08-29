0dc0dbb7a39d2fa285128d8f34d77f4188d1157e
shpunk <ignas.vaitkus7@gmail.com>
2022-08-29 08:46:39 +0300

    Removed useEffect and useState imports

--- src/Components/ToDoItem/ToDoItem.js
+++ src/Components/ToDoItem/ToDoItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ToDoItem.module.css";
 import { IconButton, TextField } from "@mui/material";
